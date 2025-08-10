8ea3253c329e2a9b74b27b30c5e94b489c45114e
YSYSYS66 <43196369+YSYSYS66@users.noreply.github.com>
2025-08-10 16:24:35 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,13 @@
+export const contentConfig = `import { defineCollection, z } from 'astro:content';
 
+const posts = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    summary: z.string().optional(),
+    tags: z.array(z.string()).default([]),
+    publishDate: z.date()
+  })
+});
+
+export const collections = { posts };`;
