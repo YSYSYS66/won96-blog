e527921ff3903fc193642c4b84dd51c50ebbe9ed
YSYSYS66 <43196369+YSYSYS66@users.noreply.github.com>
2025-08-10 16:08:29 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,10 @@
+export const astroConfig = `import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://blog.won96.com',
+  integrations: [tailwind({ applyBaseStyles: true }), sitemap()],
+  build: { inlineStylesheets: 'auto' }
+});`;
 
