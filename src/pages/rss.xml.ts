e2f803e11075ce3a26199fc1e6eeb2d067f1424d
YSYSYS66 <43196369+YSYSYS66@users.noreply.github.com>
2025-08-10 16:28:30 +0800

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,1 +1,17 @@
+export const rss = `import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
+export async function GET(context) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: 'WON96 Blog',
+    description: 'WON96 官方博客',
+    site: context.site,
+    items: posts.map((p) => ({
+      title: p.data.title,
+      description: p.data.summary,
+      pubDate: p.data.publishDate,
+      link: '/posts/' + p.slug
+    }))
+  });
+}`;
